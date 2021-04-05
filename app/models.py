fd669d8d048987936080df550351af979cea725a
David Matthewman <david@matthewman.org>
2021-04-05 20:31:10 +0100

    Added database models and iniital migration

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from . import db
+import uuid
 
 
 class Role(db.Model):
-    __tablename__ = 'roles'
+    __tablename__ = 'role'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role')
@@ -12,10 +13,35 @@ class Role(db.Model):
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    username = db.Column(db.Unicode(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Game(db.Model):
+    __tablename__ = 'game'
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(40), nullable=False, default=uuid.uuid4())
+    phrase = db.Column(db.UnicodeText)
+    is_active = db.Column(db.Boolean, default=False, nullable=False)
+    is_locked = db.Column(db.Boolean, default=True, nullable=False)
+    contestants = db.relationship('Contestant', backref='game')
+
+    def __repr__(self):
+        return '<Game %r>' % self.phrase
+
+
+class Contestant(db.Model):
+    __tablename__ = 'contestant'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.UnicodeText)
+    uuid = db.Column(db.String(40), nullable=False, default=uuid.uuid4())
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    score = db.Column(db.Integer, default=0)
+
+    def __repr__(self):
+        return '<Contestant %r>' % self.name
